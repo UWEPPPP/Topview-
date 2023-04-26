84493e9342629a3f007b679e290e612a871722f9
liu <a2506991786@qq.com>
2023-04-27 01:06:23 +0800

    feat:编写了部分合约

---
+++ SecondRound/src/main/resources/contract/Task/MyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "Task/ERC1155.sol";
+
+contract NftMarket is ERC1155 {
+    uint256 private tokenId=0;
+    constructor () ERC1155("https//ipfs.io/ipfs") public{
+
+    }
+
+    function createToken()
+}
